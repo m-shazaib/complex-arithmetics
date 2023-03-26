1e01ffb8b4f47316e06d06764cb78c6c9af314e0
01-134222-100 <01-134222-100@student.bahria.edu.pk>
2023-03-26 14:49:35 +0500

    modified the whole program. Previously two objects had to be made to perorm complex operations. But now modified the whole program, only 1 object can be used to do the whole operation.

--- Fake.h
+++ Fake.h
@@ -5,42 +5,94 @@ using namespace std;
 class complex
 {
 public:
-	complex() {
-		num1 = 0;
-		num2 = 0;
-	}
-	complex(int n1, int n2)
+	//default constructor
+	complex(float n1=0, float n2=0,float n3=0, float n4=0)
 	{
-		num1 = n1;
-		num2 = n2;
+		real1 = n1;
+		real2 = n3;
+		imag1 = n2;
+		imag2 = n4;
 	}
-	void SetNum1() {
-		cout << "num1 = ";
-		cin >> num1;
+
+	//setting the data members to value through user inputs(setters)
+	void SetReal1() {
+		cout << "a in a+bi = ";
+		cin >> real1;
 		return;
 	}
-	void SetNum2() {
-		cout << "mum2 = ";
-		cin >> num2;
+	void SetImag1() {
+		cout << "b in a+bi = ";
+		cin >> imag1;
 		return;
 	}
-	float GetNum1() {
-		return num1;
+	void SetReal2() {
+		cout << "c in c+di = ";
+		cin >> real2;
+		return;
 	}
-	float GetNum2() {
-		return num2;
+	void SetImag2() {
+		cout << "d in c+di = ";
+		cin >> imag2;
+		return;
 	}
-	float addition() {
-		return num1 + num2;
+
+	//to access  the private data members (getters)
+	float GetReal1() {
+		return real1;
 	}
-	float subtraction() {
-		return num1 - num2;
+	float GetReal2() {
+		return real2;
 	}
-	float multiplication() {
-		return num1 * num2;
+	float GetImag1() {
+		return imag1;
+	}
+	float GetImag2() {
+		return imag2;
+	}
+
+	//displaying the complex number expressions as given by user
+	void display() {
+		cout <<"expression 1 : "<< real1 << "+" << imag1 << "i" << endl;
+		cout <<"expression 2 : "<< real2 << "+" << imag2 << "i" << endl;
+		return;
+	}
+
+	//to perform the operation on the complex numbers
+	char ToPerform() {
+		cout << "enter the operation to perform on the complex numbers" << endl;
+		cout << "1. addition = +" << endl;
+		cout << "2. subtraction = -" << endl;
+		cout << "3. multiplication = *" << endl;
+		cin >> operation;
+		return operation;
+	}
+
+	//arithmetic operations 
+	float realaddition() {
+		return real1+real2 ;
+	}
+	float imaginaryaddition() {
+		return imag1 + imag2;
+	}
+	float realsubtraction() {
+		return real1 - real2;
+	}
+	float imaginarysubtraction() {
+		return imag1 - imag2;
+	}
+	void multiplication() {
+		cout<<(real1*real2)+(imag1*imag2)<<"+"<<(real1*imag2)+(real2*imag1)<<"i"<<endl;
+		return;
 	}
 
 private:
-	float num1;
-	float num2;
+	float real1;//a
+	float imag1;//b
+	float real2;//c
+	float imag2;//d
+	char operation; //for storing operation to perform
+
 };
+
+//(a+bi)(c+di)
+//nighatusman031@gmail.com
--- complex.cpp
+++ complex.cpp
@@ -5,44 +5,28 @@ using namespace std;
 
 
 int main() {
-	char a;
-	complex real;
-	complex imaginary;
-
-
-	real.SetNum1();
-	real.SetNum2();
-	cout << "imaginary parts of respected numbers :" << endl;
-	imaginary.SetNum1();
-	imaginary.SetNum2();
-
-	cout << "Enter the operation you want to perform: ";
-	cin >> a;
-	switch (a)
-	{
+	
+	complex expression;
+	expression.SetReal1();
+	expression.SetImag1();
+	expression.SetReal2();
+	expression.SetImag2();
+	expression.display();
+	
+	switch (expression.ToPerform()) {
 	case '+':
-	{
-		cout << "Addition: " << real.addition() << " + " << imaginary.addition() << "i" << endl;
+		cout<<expression.realaddition()<<" + "<<expression.imaginaryaddition()<<"i" << endl;
 		break;
-	}
 	case '-':
-	{
-		cout << "Subtraction: " << real.subtraction() << " - " << imaginary.subtraction() << "i" << endl;
+		cout<<expression.realsubtraction() <<" - "<<expression.imaginarysubtraction()<<"i" << endl;
 		break;
-	}
 	case '*':
-	{
-		cout << real.multiplication() - imaginary.multiplication() << endl;
+		expression.multiplication();
 		break;
-	}
-	case '/':
-	{
-		cout << "the first expression you entered is of the form = " << real.GetNum1() << " + " << imaginary.GetNum2() << "i" << endl;
-		cout << "the second expression you entered is of the form = " << real.GetNum2() << " + " << imaginary.GetNum2() << "i";
-		break;
-	}
 	default:
-		cout << "Wrong operation";
+		cout << "Invalid input" << endl;
+		break;
 	}
+
 return 0;
 }
